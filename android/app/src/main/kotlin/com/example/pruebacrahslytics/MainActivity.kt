d239660ac6fbd98f2f3582aed041c70fdfe95d7b
heyitsgaspar <israel.chay.32@gmail.com>
2023-06-24 11:49:06 -0600

    ejemplo de funcional de crashlytics

---
+++ android/app/src/main/kotlin/com/example/pruebacrahslytics/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pruebacrahslytics
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
+  // Pass all uncaught "fatal" errors from the framework to Crashlytics
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: Center(
+          child: TextButton(
+            onPressed: () => throw Exception(),
+            child: const Text("Throw Test Exception"),
+          ),
+        ),
+      ),
+
+      //para hacer el falla corro la app y genero el fallo despues le doy a reiniciar la app
+      //y se registra el fallo.
+    );
+  }
+}
